e4ecc0dbffbb962daf3b20b411bdba81096a0e57
jcsilverio <silverio@impulsevfx.com>
2018-10-04 00:40:32 -0400

    search bar returns

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 
 class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      submit: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newSummoner = {
+      name: this.state.name
+    };
+    console.log(newSummoner);
+  }
+
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
@@ -27,21 +49,26 @@ class Navbar extends Component {
               </li>
             </ul>
 
-            <input
-              id="search-nav"
-              type="text"
-              placeholder="Search..."
-              autoComplete="off"
-            />
-            <span className="input-group-btn">
-              <button
-                className="btn-primary search-submit"
-                target="#search-nav"
-                type="button"
-              >
-                <span className="fa fa-search" />
-              </button>
-            </span>
+            <form onSubmit={this.onSubmit}>
+              <input
+                type="text"
+                placeholder="Search..."
+                name="name"
+                value={this.state.name}
+                required
+                autoComplete="off"
+                onChange={this.onChange}
+              />
+              <span className="input-group-btn">
+                <button
+                  className="btn-primary search-submit"
+                  target="#search-nav"
+                  type="button"
+                >
+                  <span className="fa fa-search" />
+                </button>
+              </span>
+            </form>
           </div>
         </div>
       </nav>
@@ -51,4 +78,20 @@ class Navbar extends Component {
 
 export default Navbar;
 
-// class="form-control search-input ui-autocomplete-input"
+{
+  /* <input
+id="search-nav"
+type="text"
+placeholder="Search..."
+autoComplete="off"
+/>
+<span className="input-group-btn">
+<button
+  className="btn-primary search-submit"
+  target="#search-nav"
+  type="button"
+>
+  <span className="fa fa-search" />
+</button>
+</span> */
+}
--- client/src/components/layout/Stats.js
+++ client/src/components/layout/Stats.js
@@ -37,9 +37,7 @@ class Stats extends Component {
           <div className="row">
             <div className="col-md-12">
               <h1 className="display-4 text-center">Summoner Stats</h1>
-              <p className="lead text-center">
-                League of Legends Stats Viewing App
-              </p>
+              <p className="lead text-center">League of Legends Stats Viewer</p>
             </div>
             <div className="row">
               <div className="col-lg-3" />
