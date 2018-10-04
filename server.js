c1902d844de13a30fae4972e9d4a0cf09c92b587
jcsilverio <silverio@impulsevfx.com>
2018-10-03 21:55:29 -0400

    server and client run concurrently

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const summs = require("./routes/api/summs");
 // Use Routes
 app.use("/api/summs", summs);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
