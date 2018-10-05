f77cf83303c6c37549b4dc5ae008416f39b35f07
jcsilverio <silverio@impulsevfx.com>
2018-10-04 23:18:33 -0400

    server update for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const summs = require("./routes/api/summs");
 
 const bodyParser = require("body-parser");
+const path = require("path");
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,6 +20,16 @@ app.use((req, res, next) => {
 
 // Use Routes
 app.use("/api/summs", summs);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
