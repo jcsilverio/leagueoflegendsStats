64668c4c0041bd0aad1dae974b96fc3f31b4c8b7
jcsilverio <silverio@impulsevfx.com>
2018-10-05 00:31:27 -0400

    summoner formatting

--- client/src/components/content/Summoner.js
+++ client/src/components/content/Summoner.js
@@ -33,7 +33,7 @@ class Summoner extends Component {
       this.state.profileIconId
     }.png`;
     return (
-      <div className="profiles">
+      <div>
         <div className="container">
           <div className="row">
             <div className="col-md-12">
@@ -42,25 +42,28 @@ class Summoner extends Component {
                 League of Legends Stats Viewing App
               </p>
             </div>
-            <div className="row">
-              <div className="col-lg-3" />
-              <div className="col-lg-3">
-                <img
-                  className="rounded-circle img-thumbnail summ-thumbnail float-right"
-                  src={summImage}
-                  alt="summoner profile pic"
-                />
-              </div>
-              <div className="col-lg-3">
-                <h3>{this.state.name}</h3>
-                <p>Id: {this.state.id}</p>
-                <p>profileid: {this.state.profileIconId}</p>
-                <a href="profile.html" className="btn btn-info">
-                  View Profile
-                </a>
-              </div>
-              <div className="col-lg-3" />
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3" />
+            <div className="col-lg-3">
+              <img
+                className="rounded-circle img-thumbnail summ-thumbnail float-right"
+                src={summImage}
+                alt="summoner profile pic"
+              />
             </div>
+            <div className="col-lg-3">
+              <h3>{this.state.name}</h3>
+              <p>Placeholder info</p>
+              <p>Placeholder info 2</p>
+              <a href="profile.html" className="btn btn-info">
+                View Profile
+              </a>
+            </div>
+            <div className="col-lg-3" />
           </div>
         </div>
       </div>
