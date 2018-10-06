bf4e7be6fa65409a10a2019892b8cd36e35333b9
jcsilverio <silverio@impulsevfx.com>
2018-10-05 21:37:06 -0400

    summs route loads

--- routes/api/summs.js
+++ routes/api/summs.js
@@ -9,26 +9,24 @@ require("dotenv").config();
 
 // Load Summoner model
 
-// router.get("/", function(req, res, next) {
-//   var api_key = process.env.API_KEY;
-//   var input_toSearch = "RiotSchmick";
-//   var URL =
-//     "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" +
-//     input_toSearch +
-//     "?api_key=" +
-//     api_key;
+router.get("/", function(req, res, next) {
+  var api_key = process.env.API_KEY;
+  var input_toSearch = "RiotSchmick";
+  var URL =
+    "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" +
+    input_toSearch +
+    "?api_key=" +
+    api_key;
 
-//   axios
-//     .get(URL)
-//     .then(response => {
-//       console.log(response.data);
-//       res.send(response.data);
-//     })
-//     .catch(error => {
-//       console.log("Error fetching and parsing data", error);
-//     });
-// });
-
-// });
+  axios
+    .get(URL)
+    .then(response => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.log("Error fetching and parsing data", error);
+    });
+});
 
 module.exports = router;
