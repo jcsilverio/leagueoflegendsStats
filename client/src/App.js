5d9f1479360d20a6aacc28609ab70792994d7636
jcsilverio <silverio@impulsevfx.com>
2018-10-08 23:17:03 -0400

    remove slice limitation. all matches render

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
           axios
             .get(`/api/matches/log/${this.state.summoner.accountId}`)
             .then(res => {
-              this.setState({ matches: res.data.matches.slice(0, 10) });
+              this.setState({ matches: res.data.matches });
             })
             .then(res => {
               this.state.matches.map((match, index) => {
